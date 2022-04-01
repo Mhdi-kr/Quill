e67b911eb1111ce747d1f12e21ff10261f8ed298
Mahdi  Karimi <mhdi_kr@outlook.com>
2022-04-02 03:43:30 +0430

    feat: persist content using local storage

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,32 @@
-var quill = new Quill('#quill-container', {
-  modules: {
-    toolbar: [
-      [{ header: [1, 2, false] }],
-      ['bold', 'italic', 'underline'],
-      ['image', 'code-block']
-    ]
-  },
-  scrollingContainer: '#scrolling-container', 
-  placeholder: 'Start writing...',
-  theme: 'bubble'
+var quill = new Quill("#quill-container", {
+    modules: {
+        toolbar: [
+            [{ header: [1, 2, false] }],
+            ["bold", "italic", "underline"],
+            ["image", "code-block"],
+        ],
+    },
+    scrollingContainer: "#scrolling-container",
+    placeholder: "Start writing...",
+    theme: "bubble",
+});
+
+quill.on("text-change", function (delta, oldDelta, source) {
+    if (source === "user") {
+        window.localStorage.setItem(
+            "quill-content",
+            JSON.stringify(quill.editor?.delta?.ops)
+        );
+    }
+});
+
+window.addEventListener("load", () => {
+    try {
+        const savedContent = JSON.parse(
+            window.localStorage.getItem("quill-content")
+        );
+        quill.setContents(savedContent);
+    } catch (err) {
+        alert("Error loading content");
+    }
 });
